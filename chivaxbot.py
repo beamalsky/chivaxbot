4c1fccf9917d15fa29d67db37bf8721e870557c3
Bea Malsky <beamalsky@datamade.us>
2021-07-05 19:44:40 -0500

    Working gif generation

--- chivaxbot.py
+++ chivaxbot.py
@@ -5,7 +5,7 @@ import pytz
 
 from datetime import date, datetime, timedelta
 
-from utils import vax_url, deaths_url, get_json, chicago_zips, get_colors_dict, write_svg
+from utils import vax_url, deaths_url, get_json, get_vax_perc_by_date, chicago_zips, get_colors_dict, write_svg
 
 vax_svg_path = os.path.join(os.getcwd(), "data", "zipcodes-vax.svg")
 deaths_svg_path = os.path.join(os.getcwd(), "data", "zipcodes-deaths.svg")
@@ -15,93 +15,82 @@ deaths_colorscale =  ["#feebe2", "#f3cea3", '#f3b875', '#C83302', '#992702']
 
 now = datetime.now(pytz.timezone('America/Chicago'))
 vax_output_path = os.path.join(os.getcwd(), "exports", "vax-{}.png".format(
-    now.strftime("%Y-%m-%d-%H%M")
+	now.strftime("%Y-%m-%d-%H%M")
 ))
 vax_output_path_latest = os.path.join(os.getcwd(), "exports", "vax-latest.png")
 
 deaths_output_path = os.path.join(os.getcwd(), "exports", "deaths-{}.png".format(
-    now.strftime("%Y-%m-%d-%H%M")
+	now.strftime("%Y-%m-%d-%H%M")
 ))
 deaths_output_path_latest = os.path.join(os.getcwd(), "exports", "deaths-latest.png")
 
 sentence_output_path_latest = os.path.join(os.getcwd(), "exports", "sentence-latest.json")
 
 def get_tweet():
-    vax_res_json = get_json(vax_url)
-    deaths_res_json = get_json(deaths_url)
+	vax_res_json = get_json(vax_url)
+	deaths_res_json = get_json(deaths_url)
 
-    # loop over city data and return a dictionary of zipcodes and their vax rates
-    # also, a sum of all vaccinations
-    vax_perc = {}
-    vax_sum = 0
-    population_sum = 0
-    max_date = max([datetime.strptime(i[9], '%Y-%m-%dT00:00:00') for i in vax_res_json["data"]])
-    for row in vax_res_json["data"]:
-        # we only want dose cumulatives from the latest date
-        # res date should be in the format 2021-01-18T00:00:00
-        if max_date == datetime.strptime(row[9], '%Y-%m-%dT00:00:00'):
-            vax_perc[row[8]] = float(row[17])
-            vax_sum += int(row[16])
-            population_sum += int(row[27])
+	max_date = max([datetime.strptime(i[9], '%Y-%m-%dT00:00:00') for i in vax_res_json["data"]])
+	vax_dict = get_vax_perc_by_date(vax_res_json, max_date, save_totals=True)
 
-    deaths_perc = {}
-    deaths_sum = 0
-    max_week = max([int(i[9]) for i in deaths_res_json["data"]])
-    for row in deaths_res_json["data"]:
-        if max_week == int(row[9]):
-            # take "death rate per 100,000 population through the week"
-            deaths_perc[row[8]] = float(row[25])
-            deaths_sum += int(row[23])
+	deaths_perc = {}
+	deaths_sum = 0
+	max_week = max([int(i[9]) for i in deaths_res_json["data"]])
+	for row in deaths_res_json["data"]:
+		if max_week == int(row[9]):
+			# take "death rate per 100,000 population through the week"
+			deaths_perc[row[8]] = float(row[25])
+			deaths_sum += int(row[23])
 
-    as_of = now.strftime("%B %-d, %Y") # January 2, 2021
-    vaccinations = f'{vax_sum:,}'
-    percent_vaccinated = round(vax_sum / population_sum * 100, 1)
-    tweet_text = "As of {date}, Chicago is reporting {vaccinations} people fully vaccinated: {percent}% of the population.\n\nWho is dying:           Who is vaccinated:".format(
-        date=as_of,
-        vaccinations=vaccinations,
-        percent=percent_vaccinated,
-    )
+	as_of = now.strftime("%B %-d, %Y") # January 2, 2021
+	vaccinations = f'{vax_dict["vax_sum"]:,}'
+	percent_vaccinated = round(vax_dict["vax_sum"] / vax_dict["population_sum"] * 100, 1)
+	tweet_text = "As of {date}, Chicago is reporting {vaccinations} people fully vaccinated: {percent}% of the population.\n\nWho is dying:		   Who is vaccinated:".format(
+		date=as_of,
+		vaccinations=vaccinations,
+		percent=percent_vaccinated,
+	)
 
-    write_sentence_json(sentence_output_path_latest, {
-        "as_of": as_of, 
-        "vaccinations": vaccinations,
-        "percent_vaccinated": percent_vaccinated,
-    })
+	write_sentence_json(sentence_output_path_latest, {
+		"as_of": as_of,
+		"vaccinations": vaccinations,
+		"percent_vaccinated": percent_vaccinated,
+	})
 
-    # then, create a dictionary of zip codes and colors
-    vax_colors = get_colors_dict(vax_perc, vax_colorscale, "vax")
-    deaths_colors = get_colors_dict(deaths_perc, deaths_colorscale, "deaths")
+	# then, create a dictionary of zip codes and colors
+	vax_colors = get_colors_dict(vax_dict["vax_perc"], vax_colorscale, "vax")
+	deaths_colors = get_colors_dict(deaths_perc, deaths_colorscale, "deaths")
 
-    write_svg(
-        vax_svg_path,
-        [vax_output_path, vax_output_path_latest],
-        vax_colors
-    )
-    write_svg(
-        deaths_svg_path,
-        [deaths_output_path, deaths_output_path_latest],
-        deaths_colors
-    )
+	write_svg(
+		vax_svg_path,
+		[vax_output_path, vax_output_path_latest],
+		vax_colors
+	)
+	write_svg(
+		deaths_svg_path,
+		[deaths_output_path, deaths_output_path_latest],
+		deaths_colors
+	)
 
-    alt_text = '''
-    Two maps of Chicago, side by side. The map on the left shows COVID-19 deaths
-    per capita by ZIP code. The map on the right shows completed COVID-19
-    vaccination per capita by ZIP code. The maps reveal a disconnect between
-    where residents are getting vaccinated and where COVID-19 deaths are
-    concentrated.
-    '''
+	alt_text = '''
+	Two maps of Chicago, side by side. The map on the left shows COVID-19 deaths
+	per capita by ZIP code. The map on the right shows completed COVID-19
+	vaccination per capita by ZIP code. The maps reveal a disconnect between
+	where residents are getting vaccinated and where COVID-19 deaths are
+	concentrated.
+	'''
 
-    return {
-        "tweet_text": tweet_text,
-        "deaths_map_path": deaths_output_path,
-        "vax_map_path": vax_output_path,
-        "alt_text": alt_text,
-        "deaths_map_path_latest": deaths_output_path_latest,
-        "vax_map_path_latest": vax_output_path_latest,
-        "sentence_path_latest": sentence_output_path_latest,
-    }
+	return {
+		"tweet_text": tweet_text,
+		"deaths_map_path": deaths_output_path,
+		"vax_map_path": vax_output_path,
+		"alt_text": alt_text,
+		"deaths_map_path_latest": deaths_output_path_latest,
+		"vax_map_path_latest": vax_output_path_latest,
+		"sentence_path_latest": sentence_output_path_latest,
+	}
 
 def write_sentence_json(output_path, dict):
-    with open(output_path, 'w') as json_file:
-        json.dump(dict, json_file)
-        print("Saved sentence file {}".format(output_path))
+	with open(output_path, 'w') as json_file:
+		json.dump(dict, json_file)
+		print("Saved sentence file {}".format(output_path))
--- chivaxbot_gif.py
+++ chivaxbot_gif.py
@@ -4,36 +4,71 @@ import glob
 
 from datetime import date, datetime, timedelta
 from PIL import Image
+from utils import vax_url, get_json, get_vax_perc_by_date, get_colors_dict_absolute, write_svg
 
-png_input_paths = os.path.join(os.getcwd(), "exports", "gif", "vax-*.png")
-now = datetime.now(pytz.timezone('America/Chicago'))
+gif_svg_path = os.path.join(os.getcwd(), "data", "zipcodes-vax-for-gif.svg")
+
+exports_gif_dir = os.path.join(os.getcwd(), "exports", "gif")
+png_input_paths = os.path.join(exports_gif_dir, "vax-*.png")
+now = datetime.now()
 gif_output_path = os.path.join(
-	os.getcwd(),
-	"exports",
-	"gif",
+	exports_gif_dir,
 	"gif-{}.gif".format(now.strftime("%Y-%m-%d-%H%M"))
 )
 
+start_date = '2021-01-25'
+gif_colorscale = ["#900552", "#c7207d", "#df78af", "#f2b6db", "#fee1ef", "#e8f6cf", "#b9e087", "#7fbb41", "#4e9121", "#286419"]
+
 def get_gif_tweet():
+	# clear out gif folder (to make local testing easier)
+	for f in os.listdir(exports_gif_dir):
+		if f != ".gitkeep":
+			os.remove(os.path.join(exports_gif_dir, f))
+
 	# generate still images with alphabetical names
-  vax_res_json = get_json(vax_url)
-	deaths_res_json = get_json(deaths_url)
+	# set starting date and then incremement by 7 until we hit today
+	vax_res_json = get_json(vax_url)
+	date = datetime.strptime(start_date, '%Y-%m-%d')
+	while date <= now:
+		vax_perc = get_vax_perc_by_date(vax_res_json, date, save_totals=False)
+		vax_colors = get_colors_dict_absolute(vax_perc, gif_colorscale, date)
+		output_path = os.path.join(
+			os.getcwd(),
+		 	"exports",
+			"gif",
+			"vax-{}.png".format(date.strftime("%Y-%m-%d"))
+		)
+
+		write_svg(
+			gif_svg_path,
+			[output_path],
+			vax_colors
+		)
+		date += timedelta(4)
 
 	# turn them into a gif
 	# https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html#gif
 	img, *imgs = [Image.open(f) for f in sorted(glob.glob(png_input_paths))]
 	img.save(
 		fp=gif_output_path,
-  	format='GIF',
+		format='GIF',
 		append_images=imgs,
 		save_all=True,
-		duration=200,
-  	loop=0
-  )
+		duration=500,
+		loop=0
+	)
 
-	# tweet them out
+	# generate the sentence and alt text too
+	tweet_text = '''
+ 		Read the latest on Chicago's widening vaccine disparity from @maerunes for @SouthSideWeekly: https://southsideweekly.com/chicagos-vaccine-disparity-widens/
+   '''
+
+  alt_text = '''
+		The animated gif shows a map of Chicago with vaccination rates for each ZIP code over time. As time passes from late January until today, the north west side pulls ahead of the rest of the city in vaccination rates.
+	'''
+
 	return {
 		"tweet_text": tweet_text,
-		"gif_path": deaths_output_path,
+		"gif_path": gif_output_path,
 		"alt_text": alt_text,
 	}
